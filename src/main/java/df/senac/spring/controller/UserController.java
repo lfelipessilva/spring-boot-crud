13f0287b6cf8c6f9d7cae5469f23e5a75bee55b7
Luís Felipe <lfelipe.silva@aterapia.com.br>
2023-05-31 20:14:23 -0300

    feature: add user crud

---
+++ src/main/java/df/senac/spring/controller/UserController.java
@@ -0,0 +1,57 @@
+package df.senac.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import df.senac.spring.database.UserRepository;
+import df.senac.spring.entity.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  @Autowired
+  private UserRepository repository;
+
+  @GetMapping
+  public List<User> list() {
+    return repository.findAll();
+  }
+
+  @PostMapping
+  public void create(@RequestBody User user) {
+    repository.save(user);
+  }
+
+  @PutMapping
+  public void update(@RequestBody User user) {
+    if (user.getId() > 0) {
+      repository.save(user);
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
+
+  @GetMapping(value="findbyid")
+  public User findById(@RequestParam(name="userId") Long userId) {
+    return repository.findById(userId).get();
+  }
+
+  @GetMapping(value="findbyname")
+  public List<User> findByName(@RequestParam(name="name") String name) {
+    return repository.findByName(name);
+  }
+}
---
+++ src/main/java/df/senac/spring/entity/User.java
@@ -0,0 +1,45 @@
+package df.senac.spring.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private Number age;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String nome) {
+    this.name = nome;
+  }
+
+  public Number getAge() {
+    return this.age;
+  }
+
+  public void setAge(Number idade) {
+    this.age = idade;
+  }
+
+}
